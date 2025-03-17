103f44f79f61fe4934eb972539a807c94de60c02
NHHoangTuan <hoangtuan.study@gmail.com>
2025-03-17 23:48:53 +0700

    fix bug booking

--- src/main/java/petitus/petcareplus/configuration/RedisConfig.java
+++ src/main/java/petitus/petcareplus/configuration/RedisConfig.java
@@ -1,6 +1,8 @@
 package petitus.petcareplus.configuration;
 
 import io.github.cdimascio.dotenv.Dotenv;
+
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -16,25 +18,28 @@ public class RedisConfig {
 
     private static final Dotenv dotenv = Dotenv.load();
 
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
+    // @Bean
+    // public RedisConnectionFactory redisConnectionFactory() {
 
-        String host = dotenv.get("REDIS_HOST", "");
-        int port = Integer.parseInt(dotenv.get("REDIS_PORT", "6380"));
-        String password = dotenv.get("REDIS_PASSWORD", "");
+    // String host = dotenv.get("REDIS_HOST", "");
+    // int port = Integer.parseInt(dotenv.get("REDIS_PORT", "6380"));
+    // String password = dotenv.get("REDIS_PASSWORD", "");
 
-        // Cấu hình kết nối Redis với SSL
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
-        if (!password.isEmpty()) {
-            redisStandaloneConfiguration.setPassword(password);
-        }
+    // // Cấu hình kết nối Redis với SSL
+    // RedisStandaloneConfiguration redisStandaloneConfiguration = new
+    // RedisStandaloneConfiguration(host, port);
+    // if (!password.isEmpty()) {
+    // redisStandaloneConfiguration.setPassword(password);
+    // }
 
-        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-                .useSsl()
-                .build();
+    // LettuceClientConfiguration clientConfig =
+    // LettuceClientConfiguration.builder()
+    // .useSsl()
+    // .build();
 
-        return new LettuceConnectionFactory(redisStandaloneConfiguration, clientConfig);
-    }
+    // return new LettuceConnectionFactory(redisStandaloneConfiguration,
+    // clientConfig);
+    // }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
@@ -43,5 +48,12 @@ public class RedisConfig {
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
+    }
+
+    @Bean
+    public ApplicationRunner clearRedisOnStartup(RedisTemplate<String, Object> redisTemplate) {
+        return args -> {
+            redisTemplate.getConnectionFactory().getConnection().flushDb();
+        };
     }
 }
