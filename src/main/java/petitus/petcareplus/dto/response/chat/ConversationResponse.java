53b5ef8833afbc8610aa640002f2c181bbf568a7
kuwelym <klyminh@gmail.com>
2025-05-22 01:22:38 +0700

    add chat conversations fetching

---
+++ src/main/java/petitus/petcareplus/dto/response/chat/ConversationResponse.java
@@ -0,0 +1,21 @@
+package petitus.petcareplus.dto.response.chat;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+public class ConversationResponse {
+    private UUID userId;
+    private String userName;
+    private String lastMessage;
+    private String userAvatarUrl;
+    private LocalDateTime lastMessageTime;
+    private Boolean hasUnreadMessages;
+    private Long unreadCount;
+} 
