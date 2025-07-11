5e0c7ffb8349662e4c82f9d3412cbd323d51573a
kuwelym <klyminh@gmail.com>
2025-07-11 22:34:30 +0700

    update UpdateUserRequest for admin endpoint

--- src/main/java/petitus/petcareplus/dto/request/auth/UpdateUserRequest.java
+++ src/main/java/petitus/petcareplus/dto/request/auth/UpdateUserRequest.java
@@ -6,13 +6,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import petitus.petcareplus.dto.annotation.FieldMatch;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @SuperBuilder
-@FieldMatch(first = "password", second = "passwordConfirm", message = "{password_mismatch}")
 public class UpdateUserRequest {
     @Email(message = "{email_invalid}")
     private String email;
